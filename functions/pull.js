f201561c4c610813e4b0bcbe1e254baa8d5819b3
Domus <syedali.razashah1234@gmail.com>
2026-01-18 01:19:43 +0500

    Pull.js under construction

--- functions/pull.js
+++ functions/pull.js
@@ -15,8 +15,19 @@ exports.handler = async function (event, context) {
     const path = event.queryStringParameters?.path || "";
     const githubUrl = `https://api.github.com/repos/${user}/${repo}/contents/${path}`;
     
-    return {
-        statusCode: 200,
-        body: githubUrl,
-    };
+    try {
+        const response = await fetch(githubUrl);
+        return {
+            statusCode: 200,
+            body: response.ok,
+        };
+    }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: error.message,
+            }),
+        };
+    }
 };
