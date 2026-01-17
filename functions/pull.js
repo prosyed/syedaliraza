903a77093dc834e659e0690d20c36c4d0bbcaea1
Domus <syedali.razashah1234@gmail.com>
2026-01-18 00:54:56 +0500

    Pull.js under construction

--- functions/pull.js
+++ functions/pull.js
@@ -1,6 +1,9 @@
-exports.handler = async function () {
+exports.handler = async function (event, context) {
+    const user = process.env.GITHUB_USER;
+    const repo = process.env.GITHUB_REPO;
+    const token = process.env.GITHUB_TOKEN;
     return {
         statusCode: 200,
-        body: process.env,
+        body: [user, repo, token],
     };
 };
