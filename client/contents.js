9c4b4207dc82cc4796f06c47139581934df8ec52
Domus <syedali.razashah1234@gmail.com>
2026-01-21 00:48:52 +0500

    Modify the contents

--- client/contents.js
+++ client/contents.js
@@ -1,7 +1,7 @@
 const contents = {
     Prime: "<p>Prime is a 3-player parliamentary strategy web game.</p><a href='https://prime.syedaliraza.com'><b>Play now</b></a>",
     Code: "<p>The source code for all my projects.</p><p>Will be published soon.</p>",
-    Linguistics: "<p>My contributions aimed to better the understanding of languages (the natural ones):</p><ul><li>Interactive IPA Chart with occurrences</li><li>Conjugation Atlas of Arabic</li><li>Translations from Punjabi</li></ul><p>Will be published soon.</p>",
-    Essays: "<p>The stuff I write:</p><ul><li>American film from 1950 to 1959: A thematic analysis</li><li>The Comprehensive Autobiography of the Mathematical Reptile</li></ul><p>Will be published soon.</p>",
+    Essays: "<p>The stuff I write:</p><ul><li>American film from 1950 to 1959: A thematic analysis</li><li>Evolution of Wes Anderson</li><li>The Comprehensive Autobiography of the Mathematical Reptile</li></ul><p>Will be published soon.</p>",
+    Language: "<p>My publications aimed to improve the general understanding of languages (the natural ones):</p><ul><li>Interactive IPA Chart</li><li>Conjugation Atlas of Arabic</li><li>Translations from Punjabi</li></ul><p>Will be published soon.</p>",
     Myself: "<p>Hey there,<br><b>Ali</b> here, a 20 year old learner from Punjab, Pakistan. This website serves as the portal to my current and future (if Allah wills) works and publications. Stay tuned, for there is much more to come.</p><p>Reach me out at:<br><i><a href='mailto:38syed@gmail.com'>38syed@gmail.com</a></i></p>"
 }
