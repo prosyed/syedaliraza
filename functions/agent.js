77a754e481c11d7f88461485dbc5dd41911bc411
Domus <syedali.razashah1234@gmail.com>
2026-01-21 07:12:45 +0500

    Revert bad commit, functionality successful

--- functions/agent.js
+++
@@ -1,5 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-
-export async function handler(event) {
-    await neon(process.env.DATABASE_URL).query("INSERT INTO agents (id, first_seen, last_seen) VALUES (gen_random_uuid(), now() AT TIME ZONE 'UTC', now() AT TIME ZONE 'UTC')");
-}
